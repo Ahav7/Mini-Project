7c17f82d1b18904ad1bef8281f6ea381b45937b5
Hemanth Aradhyula <52327863+Ahav7@users.noreply.github.com>
2021-04-15 00:17:03 +0530

    Update edit_score.c

--- 3_Implementation/src/edit_score.c
+++ 3_Implementation/src/edit_score.c
@@ -1,1 +1,19 @@
-
+#include "head.h"
+void edit_score(float score,char playernm[100])
+{
+    system("cls");
+        float sc;
+        char nm[100];
+        FILE *f;
+        f=fopen("score.txt","r");
+        fscanf(f,"%s%f",&nm,&sc);
+        if(score >= sc)
+        {
+            sc=score;
+            fclose(f);
+            f=fopen("score.txt","w");
+            //print playername
+            fprintf(f,"%s\n%.2f",playernm,sc);
+            fclose(f);
+        }
+}
