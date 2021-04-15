4ae81d0d2d467e0e85c3a46f9ae0439b46138e71
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-15 17:47:37 +0530

    Update operations.c

--- 3_Implementation/src/operations.c
+++ 3_Implementation/src/operations.c
@@ -10,41 +10,41 @@
  */
 #include "operations.h"
 #include<math.h>
-float total_ser(float x, float y,float z)
+float total_ser(float r1, float r2,float r3)
 { 
     float b;
-    b=x+y+z;
+    b=r1+r2+r3;
     return b;
 }
-float total_par(float x, float y,float z)
+float total_par(float r1, float r2,float r3)
 {
     float c;
-    c=((x*y*z)/(x*y+y*z+z*x));
+    c=((r1*r2*r3)/(r3*r2+r1*r3+r2*r1));
     return c;
 }
-float  cal_res (float voltage, float current)
+float  cal_res (float v, float i)
 {
-    return(voltage/current);
+    return(v/i);
 }
-double unit_conv1(double resistance )
+double unit_conv1(double r )
 {
-   return resistance*pow(10,3);
+   return r*pow(10,3);
 }
-double unit_conv2(double resistance )
+double unit_conv2(double r )
 {
-    return resistance*pow(10,-3);
+    return r*pow(10,-3);
 }
-double unit_conv3(double resistance )
+double unit_conv3(double r )
 {
-    return resistance*pow(10,6);
+    return r*pow(10,6);
 }
-double unit_conv4(double resistance )
+double unit_conv4(double r )
 {
-    return resistance*pow(10,-6);
+    return r*pow(10,-6);
 }
-int res_val(int m,int n,int o,int p)
-{
-     int res;
-     res=((m*100)+(n*10)+(o))*pow(10,p);
-     return res;
-}
+int res_val(int a,int b,int c,int d)
+    {
+        int z;
+        z=((a*100)+(b*10)+(c))*pow(10,d);
+        return z;
+    }
