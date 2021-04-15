a129de89a352f7df0c3337ffb20cea355aa87fad
Hemanth Aradhyula <52327863+Ahav7@users.noreply.github.com>
2021-04-15 11:39:29 +0530

    Update head.h
    small change

--- 3_Implementation/inc/head.h
+++ 3_Implementation/inc/head.h
@@ -11,7 +11,6 @@
 #ifndef __head_h__
 #define __head_h__
 #include<stdio.h>
-#include<conio.h>
 #include<ctype.h>
 #include<stdlib.h>
 #include<string.h>
@@ -20,22 +19,23 @@
  * highest score in the records
  * 
  */
-void show_record()
+void show_record();
 /**
  * @brief resets the score 
  * by calling the function 
  * 
  */
-void reset_score()
+void reset_score();
 /**
  * @brief shows the instructions of the game
  * by calling the function
  * 
  */
-void help()
+void help();
 /**
  * @brief the score gets edited 
  * when a user plays a game more than once
  * by taking score and name as parameters
  */
-void edit_score(float,char[])
+void edit_score(float,char[]);
+#endif
