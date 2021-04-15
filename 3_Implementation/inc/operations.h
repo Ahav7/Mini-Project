70a45ec42cb867f6948b3e99b570680361e153df
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-15 17:51:04 +0530

    Update operations.h

--- 3_Implementation/inc/operations.h
+++ 3_Implementation/inc/operations.h
@@ -8,10 +8,19 @@
  * @copyright Copyright (c) 2021
  * 
  */
+/** 
+ *
+* @file operations.h
+* Resistance Calculator application with dfferent circuit operations
+*
+*/
 #ifndef __OPERATIONS_H__
 #define __OPERATIONS_H__
+
 #include <stdio.h>
 #include <stdlib.h>
+
+//  find the total resistance when connected in series
 float total_ser(float r1, float r2,float r3);
 float total_par(float r1, float r2,float r3);
 double unit_conv1(double r );
@@ -19,5 +28,12 @@ double unit_conv2(double r );
 double unit_conv3(double r );
 double unit_conv4(double r );
 float  cal_res (float v, float i);
+
+
+
+ //calculate the resistance value based on color code
+
 int res_val(int a,int b,int c,int d);
-#endif
+
+#endif  
+
