285b6e50b25a53796f965f41121a46938c881fac
Ahav7 <52327863+Ahav7@users.noreply.github.com>
2021-04-16 10:29:05 +0530

    Update operations.h

--- 3_Implementation/inc/operations.h
+++ 3_Implementation/inc/operations.h
@@ -10,9 +10,6 @@ float total_ser(float r1, float r2,float r3);
 
 //Parallel resistance
 float total_par(float r1, float r2,float r3);
-
-
-
 //Conversion
 double unit_conv1(double r );
 double unit_conv2(double r );
@@ -22,8 +19,7 @@ double unit_conv4(double r );
 //Resistance based on voltage current parameters
 float  cal_res (float v, float i);
 
-
-//Colour band resistance
+/*Colour band resistance*/
 int res_val(int a,int b,int c,int d);
 
 #endif  
